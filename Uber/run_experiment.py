e29cd7d3409883af3ecf36ea89f903cfd7c83df1
ronzadi <ronzadi@gmail.com>
2026-02-05 14:36:55 +0200

    change to grid

--- Uber/run_experiment.py
+++ Uber/run_experiment.py
@@ -30,7 +30,7 @@ def run_uber_experiment(input_csv, k, lambda_param, eps, private, gamma):
     passenger_coords = opt.process_raw_data(input_csv, "sampled_passengers.csv")
 
     # Using your optimized 2000-point, 80-column grid
-    grid_coords = opt.create_grid(n_locs=50)
+    grid_coords = opt.create_grid(n_locs=500)
     # hubs_coords = pd.DataFrame(passenger_coords).sample(500)
     # hubs_coords.to_csv('hubs_coords.csv', index=False)
     # hubs_coords = pd.read_csv("hubs_coords.csv")
@@ -85,6 +85,7 @@ def run_uber_experiment(input_csv, k, lambda_param, eps, private, gamma):
     group_2 = folium.FeatureGroup(name='All').add_to(m)
     group_1 = folium.FeatureGroup(name='Non-Private Greedy').add_to(m)
     group_3 = folium.FeatureGroup(name='DP Greedy').add_to(m)
+    group_4 = folium.FeatureGroup(name='Candidates').add_to(m)
 
     # 4. Add points from the second grid (e.g., your filtered or "on-land" grid)
     # Replace 'grid_2' with the variable name of your second coordinate array
@@ -111,6 +112,14 @@ def run_uber_experiment(input_csv, k, lambda_param, eps, private, gamma):
             color='brown',
             fill=True,
         ).add_to(group_3)
+
+    for lat, lon in grid_coords:
+        folium.CircleMarker(
+            [lat, lon],
+            radius=4,
+            color='yellow',
+            fill=True,
+        ).add_to(group_4)
 
     # 5. Add a layer control panel to the top-right
     folium.LayerControl().add_to(m)
